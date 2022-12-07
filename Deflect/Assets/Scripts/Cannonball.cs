a0f167ac101cc82eaef86b5235f7ecc91d80fe40
CR-Daniel <d.chou.rainho@student.tue.nl>
2022-12-07 15:55:23 +0100

    Point-Cloud Collision Detection

--- Deflect/Assets/Scripts/Cannonball.cs
+++ Deflect/Assets/Scripts/Cannonball.cs
@@ -12,6 +12,11 @@ public class Cannonball : MonoBehaviour
     private float dps = 1000; // degrees/sec
     private bool rotate = true;
 
+    // Collision Detection
+    private bool bladed = false;
+    private GameObject blade;
+    private GameObject[] bladeCloud;
+
     public AudioClip metalClip;
     public AudioClip poofClip;
     public AudioClip whooshClip;
@@ -25,8 +30,11 @@ public class Cannonball : MonoBehaviour
 
     private void Start()
     {
+        blade = GameObject.FindGameObjectsWithTag("Blade")[0];
+        bladeCloud = GameObject.FindGameObjectsWithTag("BladeCloud");
+
         source = GetComponent<AudioSource>();
-        srk.Rotate(0, 0, 45 * Random.Range(0, 7));
+        gameObject.transform.Rotate(0, 0, 45 * Random.Range(0, 7));
 
         // Smoke Poof SFX
         source.pitch = Random.Range(minPitch, maxPitch);
@@ -39,6 +47,23 @@ public class Cannonball : MonoBehaviour
     {
         if (!rotate) { return; }
         srk.Rotate(0, dps * Time.deltaTime, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        if (bladed) { return; }
+
+        float min = 10.0f;
+        foreach(GameObject point in bladeCloud)
+        {
+            min = Mathf.Min(min, Mathf.Abs(Vector3.Distance(point.transform.position, gameObject.transform.position)));
+        }
+
+        if(Mathf.Abs(min) < 0.2f)
+        {
+            bladed = true;
+            Collide();
+        }
     }
 
     public void Attack(Transform target, float holdTime, float speed)
@@ -54,7 +79,6 @@ public class Cannonball : MonoBehaviour
         rb = gameObject.AddComponent<Rigidbody>();
         rb.useGravity = false;
         rb.mass = 0.1f;
-        rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
         Fly();
     }
 
@@ -76,43 +100,52 @@ public class Cannonball : MonoBehaviour
         // Stop Whoosh SFX
         source.Stop();
         source.loop = false;
+        rotate = false;
+        rb.useGravity = true;
 
         if (other.tag == "Head")
         {
             other.gameObject.GetComponent<Head>().TakeLife();
             Destroy(this.gameObject);
         }
-
-        if (other.tag == "Blade")
-        {
-            rotate = false;
-            rb.useGravity = true;
-        }
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void Collide()
     {
-        if (other.gameObject.tag == "Blade")
+        rotate = false;
+        rb.useGravity = true;
+
+        // Vibration
+        blade.GetComponent<HapticBlade>().Vibrate(0.1f, 0.1f);
+
+        VelocityEstimator estimator = blade.GetComponent<VelocityEstimator>();
+        if (estimator)
         {
-            // Vibration
-            other.gameObject.GetComponent<HapticBlade>().Vibrate(0.1f, 0.1f);
+            float v = estimator.GetVelocityEstimate().magnitude;
+            double v01 = (double)Mathf.InverseLerp(minVelocity, maxVelocity, v);
+            double vc = Mathf.Clamp(v, 2.0f, 5.0f);
 
-            VelocityEstimator estimator = other.gameObject.GetComponent<VelocityEstimator>();
-            if (estimator)
-            {
-                float v = estimator.GetVelocityEstimate().magnitude;
+            // Velocity-based SFX
+            float diff = maxVolume - minVolume;
+            double volume = v01 * (double)diff + (double)minVolume;
+            source.pitch = Random.Range(minPitch, maxPitch);
+            source.PlayOneShot(metalClip, (float)volume * 1.0f);
 
-                // Velocity-based SFX
-                float diff = maxVolume - minVolume;
-                double volume = (double)Mathf.InverseLerp(minVelocity, maxVelocity, v) * (double)diff + (double)minVolume;
-                source.pitch = Random.Range(minPitch, maxPitch);
-                source.PlayOneShot(metalClip, (float)volume * 1.0f);
-
-                // Velocity-based Rebound
-                Vector3 dir = rb.velocity;
-                dir = -dir.normalized;
-                GetComponent<Rigidbody>().AddForce(dir * v * 8);
-            }
+            // Velocity-based Rebound
+            Debug.Log(rb.useGravity);
+            Vector3 dir = rb.velocity;
+            dir = -dir.normalized;
+            Debug.Log(vc);
+            rb.AddForce(dir * (float)vc * 15);
         }
     }
+
+    //void OnDrawGizmos()
+    //{
+    //    if(bladeCloud == null) { return; }
+    //    foreach (GameObject point in bladeCloud)
+    //    {
+    //        Gizmos.DrawWireSphere(point.transform.position, 0.1f);
+    //    }
+    //}
 }
